ba9dc2100c74098acbf5d94ba15ab57282936aa8
hwuhsi <58682507+hwuhsi@users.noreply.github.com>
2021-07-02 11:48:31 +0800

    fix

--- assets/js/darkmode.ts
+++ assets/js/darkmode.ts
@@ -1,19 +1,16 @@
-
-
 window.addEventListener("DOMContentLoaded", () => {
-  const theme = document.querySelector("#theme") as HTMLButtonElement;
+  const config = localStorage.getItem("theme");
 
-const config = localStorage.getItem("theme");
-
-const body = document.querySelector("body");
-
-body.dataset.theme = config === "light" ? "light" : "dark";
-
-theme.onclick = () => {
   const body = document.querySelector("body");
-  let mode = body.dataset.theme as string;
-  mode = mode === "light" ? "dark" : "light";
-  body.dataset.theme = mode;
-  localStorage.setItem("theme", mode);
-};
-})
+
+  body.dataset.theme = config === "light" ? "light" : "dark";
+
+  const theme = document.querySelector("#theme") as HTMLButtonElement;
+  theme.onclick = () => {
+    const body = document.querySelector("body");
+    let mode = body.dataset.theme as string;
+    mode = mode === "light" ? "dark" : "light";
+    body.dataset.theme = mode;
+    localStorage.setItem("theme", mode);
+  };
+});
